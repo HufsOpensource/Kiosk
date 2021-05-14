ee1381f147c6851a612785efb77f64b021b688a2
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-05-14 18:21:55 +0900

    Add(Home) : Home.java

---
+++ app/src/main/java/com/junyoung/kiosk/component/Home.java
@@ -0,0 +1,16 @@
+package com.junyoung.kiosk.component;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.junyoung.kiosk.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
